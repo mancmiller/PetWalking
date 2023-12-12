c61823e7862c51584e4dadb4583d2267ba68863c
mancmiller <manc.miller@gmail.com>
2023-12-12 14:45:43 +0600

    Added .gitignore and first setup

---
+++ PetWalking/Support/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  PetWalking
+//
+//  Created by Muslim on 12.12.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
